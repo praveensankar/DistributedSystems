19ede4217c6324fa5746fc2676528abfaf8190b1
Praveen <praveensankar000@gmail.com>
2021-09-19 16:01:31 +0200

    added flags to enable or disable cache and print to the files

--- src/ServerSimulator.java
+++ src/ServerSimulator.java
@@ -6,6 +6,8 @@ import java.rmi.server.UnicastRemoteObject;
 import java.util.concurrent.locks.ReentrantLock;
 
 class ServerSimulator {
+    // pass the flags in the following order : -c
+    // -c - to enable the cache in the server side
 
   static ReentrantLock lock = new ReentrantLock();
   static Server[] server = new Server[5];
@@ -16,8 +18,16 @@ class ServerSimulator {
         Registry registry = LocateRegistry.getRegistry();
           for(int i=0; i<5; i++) {
               server[i] = new Server(i+1);
-              // sets the cache
-              server[i].setCache(new Cache(100));
+              // sets the cache if -c flag is passed
+              if(args.length>0)
+              {
+                  if(args[0].equals("-c"))
+                  {
+                  server[i].setCache(new Cache(100));
+                  System.out.println("cache is added to the server : " +(i+1) );
+                  }
+              }
+
 
               // disables the cache
               // server[i].disableCache();
