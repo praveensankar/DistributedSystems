33a3fa551421297bff40dfee541fcd4cba2d0a3e
Praveen <praveensankar000@gmail.com>
2021-09-20 17:22:06 +0200

    added 4th method for cache

--- src/CacheInterface.java
+++ src/CacheInterface.java
@@ -2,7 +2,9 @@ public interface CacheInterface {
     public TimesPlayedTask fetchFromCache(TimesPlayedTask task);
     public TimesPlayedByUserTask fetchFromCache(TimesPlayedByUserTask task);
     public TopArtistsByUserGenreTask fetchFromCache(TopArtistsByUserGenreTask task);
+    public TopThreeMusicByUserTask fetchFromCache(TopThreeMusicByUserTask task);
     public void addToCache(TimesPlayedTask task);
     public void addToCache(TimesPlayedByUserTask task);
     public void addToCache(TopArtistsByUserGenreTask task);
+    public void addToCache(TopThreeMusicByUserTask task);
 }
