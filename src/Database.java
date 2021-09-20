4ee17fbe91b05336cd7cff441711576114bde7bc
shielakri <shiela10@msn.com>
2021-09-20 16:57:49 +0200

    Added some comments.

--- src/Database.java
+++ src/Database.java
@@ -4,10 +4,17 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+
+/**
+ * Database class.
+ *
+ * This class is responsible for querying the database.
+ * It contains all the algorithms for the different queries.
+ *
+ */
 class Database {
 
   String datafile = "../data/dataset.csv";
-  // String datafile = "../data/dummydataset.csv";
 
   int musicIndex = 0;
   int artistIndex = 1;
@@ -25,6 +32,14 @@ class Database {
   }
 
 
+  /**
+   * Executes the query 'getTimesPlayedByUser'. It takes in the task
+   * corresponding to it, the 'TimesPlayedByUserTask', queries the dataset,
+   * and places the result in the task object by using task.setResult().
+   *
+   * @param  task               The TimesPlayedByUserTask to be executed
+   * @return                    Returns the same task, updated with the result
+   */
   public TimesPlayedTask executeQuery(TimesPlayedTask task) {
 
     int count = 0;
@@ -49,11 +64,18 @@ class Database {
 
     task.setResult(count);
 
-    // Don't necessarily need to return task actually
     return task;
   }
 
 
+  /**
+   * Executes the query 'getTimesPlayed'. It takes in the task
+   * corresponding to it, the 'TimesPlayedTask', queries the dataset,
+   * and places the result in the task object by using task.setResult().
+   *
+   * @param  task               The TimesPlayedTask to be executed
+   * @return                    Returns the same task, updated with the result
+   */
   public TimesPlayedByUserTask executeQuery(TimesPlayedByUserTask task) {
 
     int count = 0;
@@ -90,6 +112,14 @@ class Database {
   }
 
 
+  /**
+   * Executes the query 'getTopThreeMusicByUser'. It takes in the task
+   * corresponding to it, the 'TopThreeMusicByUserTask', queries the dataset,
+   * and places the result in the task object by using task.setResult().
+   *
+   * @param  task               The TopThreeMusicByUserTask to be executed
+   * @return                    Returns the same task, updated with the result
+   */
   public TopThreeMusicByUserTask executeQuery(TopThreeMusicByUserTask task) {
 
     String[] top3 = new String[3];
@@ -130,6 +160,14 @@ class Database {
   }
 
 
+  /**
+   * Executes the query 'getTopArtistsByUserGenre'. It takes in the task
+   * corresponding to it, the 'TopArtistsByUserGenreTask', queries the dataset,
+   * and places the result in the task object by using task.setResult().
+   *
+   * @param  task               The TopArtistsByUserGenreTask to be executed
+   * @return                    Returns the same task, updated with the result
+   */
   public TopArtistsByUserGenreTask executeQuery(TopArtistsByUserGenreTask task) {
 
     String[] top3 = new String[3];
@@ -174,7 +212,14 @@ class Database {
 
   }
 
-  // Returns a music profile only with the first artists
+
+  /**
+   * Returns a music profile for music with id 'musicID'. If there is no
+   * such music, then it returns 'null'.
+   *
+   * @param  musicID               The music ID
+   * @return                       MusicProfile, or 'null' if not found.
+   */
   public MusicProfile getMusicProfile(String musicID) {
 
     try {
@@ -199,31 +244,15 @@ class Database {
     return null;
   }
 
-  // public UserProfile getUserProfile(String userID) {
-  //
-  //   // try {
-  //   //
-  //   //   BufferedReader br = newReader();
-  //   //   String line, delimiter = ",";
-  //   //
-  //   //   while ((line = br.readLine()) != null) {
-  //   //     String[] record = line.split(delimiter);
-  //   //
-  //   //     if (record[userIndex].equals(personID)) {
-  //   //       return new MusicProfile(musicID, record[1]);
-  //   //     }
-  //   //   }
-  //   //
-  //   //   br.close();
-  //   //
-  //   // } catch(Exception e) {
-  //   //   e.printStackTrace();
-  //   // }
-  //
-  //   return null;
-  // }
 
-  // Performing a simple insertion sort for each entry
+  /**
+   * A helper function that returns the keys that map to
+   * the highest three values. The keyes are sorted on their values.
+   * Highest key is first, lowest key is last.
+   *
+   * @param  map              The HashMap<String, Integer> to be sorted.
+   * @return                  String array with the keys pointing to highest values.
+   */
   private String[] getTop3(HashMap<String, Integer> map) {
 
     String[] top3 = new String[3];
@@ -258,6 +287,8 @@ class Database {
   public static void main(String[] args) {
 
     Database db = new Database();
+
+    db.datafile = "../data/dummydataset.csv";
 
     System.out.println("\nDummy dataset");
 
