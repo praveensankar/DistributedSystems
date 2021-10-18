34655bf925ae078ece54bcb541572133bd39cb21
shielakri <shiela10@msn.com>
2021-10-18 15:36:28 +0200

    Some cleanup

--- assignment2/src/Listener.java
+++ assignment2/src/Listener.java
@@ -4,64 +4,55 @@ import java.sql.SQLOutput;
 import java.util.ArrayList;
 
 public class Listener implements AdvancedMessageListener {
-  @Override
-  public void regularMessageReceived(SpreadMessage message) {
-      System.out.println("test");
-      try {
-          System.out.println("message : " + message.getObject().toString() + "\t sent by replica : "+
-                  message.getSender().toString());
 
-          Transaction transaction = (Transaction) message.getObject();
-          String cmd = transaction.getCommand();
+    @Override
+    public void regularMessageReceived(SpreadMessage message) {
+        try {
 
-          if (cmd.equals("exit")) {
-              System.out.println("\nEXITING\n");
-          //   String id = message.getSender().toString().split("#")[1];
-          // //   System.out.println("id = " + id);
-          //   if (AccountReplica.replicaId.equals(id)) {
-              AccountReplica.removeTransactionFromOutstandingCollection();
-              AccountReplica.exit();
-            // }
-          }
-          else if (cmd.equals("getSyncedBalance")) {
+            System.out.print("message : " + message.getObject().toString());
+            System.out.print("\t sent by replica : " + message.getSender().toString() + "\n");
 
-            // String id = message.getSender().toString().split("#")[1];
-            // //
-            // // System.out.println("id = " + id);
-            // if (AccountReplica.replicaId.equals(id)) {
-              AccountReplica.getQuickBalance();
-              AccountReplica.removeTransactionFromOutstandingCollection();
-            // }
+            Transaction transaction = (Transaction) message.getObject();
+            String cmd = transaction.getCommand();
 
-          }
-          else if (cmd.startsWith("deposit")) {
-              double amount = Double.parseDouble(cmd.split(" ")[1]);
-              AccountReplica.deposit(amount);
+            if (cmd.equals("exit")) {
 
-              AccountReplica.removeTransactionFromOutstandingCollection();
-              AccountReplica.addTransactionToExecutedList(transaction);
+                System.out.println("\nEXITING\n");
+                AccountReplica.removeTransactionFromOutstandingCollection();
+                AccountReplica.exit();
 
-          }
-          else if (cmd.startsWith("addInterest")) {
-              double interestRate = Double.parseDouble(cmd.split(" ")[1]);
-              AccountReplica.addInterest(interestRate);
+            } else if (cmd.equals("getSyncedBalance")) {
 
-              AccountReplica.removeTransactionFromOutstandingCollection();
-              AccountReplica.addTransactionToExecutedList(transaction);
-          }
-          else {
-              System.out.println("invalid command");
-          }
+                AccountReplica.getQuickBalance();
+                AccountReplica.removeTransactionFromOutstandingCollection();
 
+            } else if (cmd.startsWith("deposit")) {
 
-      } catch (SpreadException e) {
-          e.printStackTrace();
-      }
-  }
+                double amount = Double.parseDouble(cmd.split(" ")[1]);
+                AccountReplica.deposit(amount);
 
-  @Override
-  public void membershipMessageReceived(SpreadMessage message) {
-      System.out.println("\nmembershipMessageReceived\n");
-      AccountReplica.updateMembers(message.getMembershipInfo().getMembers());
-  }
+                AccountReplica.removeTransactionFromOutstandingCollection();
+                AccountReplica.addTransactionToExecutedList(transaction);
+
+            } else if (cmd.startsWith("addInterest")) {
+
+                double interestRate = Double.parseDouble(cmd.split(" ")[1]);
+                AccountReplica.addInterest(interestRate);
+
+                AccountReplica.removeTransactionFromOutstandingCollection();
+                AccountReplica.addTransactionToExecutedList(transaction);
+
+            } else {
+                System.out.println("invalid command");
+            }
+
+        } catch (SpreadException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void membershipMessageReceived(SpreadMessage message) {
+        AccountReplica.updateMembers(message.getMembershipInfo().getMembers());
+    }
 }
