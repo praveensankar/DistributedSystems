5355a975cff9c49ddaed0b4772c6ba4be1053185
shielakri <shiela10@msn.com>
2021-10-27 12:22:56 +0200

    Restructuring and cleanup.

--- assignment3/peersim-1.0.5/doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"peersim"},{"l":"peersim.cdsim"},{"l":"peersim.config"},{"l":"peersim.core"},{"l":"peersim.dynamics"},{"l":"peersim.edsim"},{"l":"peersim.graph"},{"l":"peersim.rangesim"},{"l":"peersim.reports"},{"l":"peersim.transport"},{"l":"peersim.util"},{"l":"peersim.vector"}]
---
+++ assignment3/peersim-1.0.5/src/p2p/gossip/MessageType.java
@@ -0,0 +1,5 @@
+package p2p.gossip;
+
+public enum MessageType {
+	SHUFFLE_REQUEST, SHUFFLE_REPLY, SHUFFLE_REJECTED
+}
